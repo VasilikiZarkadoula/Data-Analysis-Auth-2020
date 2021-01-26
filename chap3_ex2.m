7f83fedcd932f845e2af1ce61f2afc7c8db7ed40
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-26 20:23:33 +0200

    Update chap3_ex2.m

--- chap3_ex2.m
+++ chap3_ex2.m
@@ -6,51 +6,61 @@
 clc;
 clear;
 
-%a)
+%a) show that the mle estimator of samples is equal to the mean value of 
+% the samples
 
  % random choise of mu and n
 mu = 1; 
 n = 10000;  
 
     % first method
-x = zeros(1,n);
-for i=1:n
-    x(i) = exprnd(mu);
-end
-m = mean(x);
+samples = exprnd(mu,n,1);
+m = mean(samples);
 fprintf('mu = %i and mean(samples) = %3f\n',mu,m);
 
     % second method
-ml = mle(x,'distribution','exp');
+ml = mle(samples,'distribution','exp');
 fprintf('mu = %i and mle result = %3f\n',mu,ml);
+
+if( abs(m - ml) < 0.0001 )
+    fprintf('mle(samples) = mean(samples)\n');
+end
 
 %--------------------------------------------------------------------------
 
 %b)
 
-M = 5000;
+M = 10000;
 n = 500;
 mu = 4;
-average = mean_Exponential(mu,M,n);
+average = meanExponential(mu,M,n);
+fprintf('\nmu = %0.3f\n',mu);
+fprintf('Average = %0.3f\n',average);
 
-function avrg = mean_Exponential(mu,samples,size)
-x = zeros(samples,size);
-for i=1:samples
-    for j=1:size
-         x(i,j) = exprnd(mu);
-    end
-end
-m = mean(x,2);
+function avrg = meanExponential(mu,numOfSamples,sizeOfSamples)
+
+samples = exprnd(mu,sizeOfSamples,numOfSamples);
+m = mean(samples,2);
 avrg = mean(m);
 
 figure;
 h = histogram(m);
-    % check if center of histogram is mu
+hold on
+% check if center of histogram is mu
 lim = h.BinLimits;
 center = (lim(1) + lim(2))/2;
+p1 = plot([center center],ylim,'r');
 hold on
-plot([center center],ylim,'r');
+p2 = plot([mu mu],ylim,'y');
+title('Histogram of the mean values of samples from Poisson distribution')
+legend([p1 p2],{'Histogram Center','mu'})
 hold off
+
+if( abs(center - mu) < 0.001 )
+    fprintf('\nHistogram center = mu = %i\n',mu);
+else
+    fprintf('\nHistogram center not equal to lamda\n');
+end
 end
 
 
