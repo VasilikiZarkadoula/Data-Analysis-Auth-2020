ef20f0cd202d2882029479da9ae2541c34bd12b6
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-26 17:01:55 +0200

    Update chap2_ex3.m

--- chap2_ex3.m
+++ chap2_ex3.m
@@ -1,28 +1,34 @@
 % Vasia Zarkadoula
 % Data Analysis 2020
 % Chapter 2 Excerise 3
-% Check that Var[x+y]=Var[x]+Var[y] does not hold when X,Y are correlated
+% Check that Var[x+y]=Var[x]+Var[y] does not hold when X,Y are not independent
 
+close all;
 clc;
 clear;
 
 n = 10000;
 mu = [0 0];
-sigmaX = 1;
-sigmaY = 1;
-p = 0.8; % random correlation coefficient
+sigma1 = 1;
+sigma2 = 1;
+p = 0.5;    % random correlation coefficient
 
-sigmaXY = p*sigmaX*sigmaY;
-Cov = [sigmaX sigmaXY; sigmaXY sigmaY];
+sigma12 = p*sigma1*sigma2;
+covMatrix = [sigma1 sigma12; ...
+             sigma12 sigma2];
 
-r = mvnrnd(mu,Cov,n); % Multivariate Normal Distribution
+r = mvnrnd(mu,covMatrix,n);     % Multivariate normal random numbers
+X = r(:,1);
+Y = r(:,2);
 
-varX = var(r(:,1));
-varY = var(r(:,2));
-varXY = var(r(:,1) + r(:,2));
-sumVar = varX+varY;
+varX = var(X);
+varY = var(Y);
+varXY = var(X+Y);
+sumVar = varX + varY;
 
 fprintf('Var[X+Y] =% 3.3f | Var[X] + Var[Y] = %3.3f \n',varXY,sumVar);
 if sumVar ~= varXY
-    fprintf('Var[x+y] not equal to Var[x]+Var[y]\n');
+    fprintf('Var[x+y] is not equal to Var[x] + Var[y]\n');
+else
+    fprintf('Var[x+y] is equal to Var[x] + Var[y]\n');
 end
