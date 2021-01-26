a2b1acfb88f10ad68b72945db3ac0167fb332c32
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-26 16:32:01 +0200

    Update chap2_ex1.m

--- chap2_ex1.m
+++ chap2_ex1.m
@@ -1,23 +1,26 @@
 % Vasia Zarkadoula
 % Data Analysis 2020
 % Chapter 2 Excerise 1
-% Generation of discrete uniform data and computation of the occurrence of a discrete value.
+% Generation of discrete uniform data and computation of the occurrence of 
+% a discrete value.
 % Flipping coin
 
+close all;
 clc;
 clear;
 
-n = [10 100 1000 10000];
-N = categorical({'10','100','1000','10000'});
-N = reordercats(N,{'10','100','1000','10000'});
+n = [10 100 500 1000 2500 5000 7500 10000];
+N = categorical(n);
 
 % Flipping coin - Uniformly distributed random numbers
-heads = 0;
-tails = 0;
+
 numofHeads = zeros(length(n),1);
 numofTails = zeros(length(n),1);
 tailsRatio = zeros(length(n),1);
 for j = 1:length(n)
+    heads = 0;
+    tails = 0;
+    % Flip the coin n times
     for i = 1:n(j)
         x = rand;
         if x < 0.5
@@ -34,17 +37,21 @@ end
 figure(1);
 bar(N,tailsRatio);
 xlabel('Number of iterations (n) of flipping coin');
-ylabel('Tails ration in n iterations')
+ylabel('Tails ratio in n iterations')
 ylim([0 1])
+
+%--------------------------------------------------------------------------
 
 % Flipping coin - Random numbers from discrete uniform distribution
 % 1 for heads, 2 for tails
-heads = 0;
-tails = 0;
+
 numofHeads = zeros(length(n),1);
 numofTails = zeros(length(n),1);
 tailsRatio = zeros(length(n),1);
 for j = 1:length(n)
+    heads = 0;
+    tails = 0;
+    % Flip the coin n times
     for i = 1:n(j)
         x = unidrnd(2);
         if x == 1
