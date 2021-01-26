3398bd8276f4748cbac204c79ae5f384eda6fd86
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-26 16:35:27 +0200

    Update chap2_ex2.m

--- chap2_ex2.m
+++ chap2_ex2.m
@@ -1,17 +1,19 @@
 % Vasia Zarkadoula
 % Data Analysis 2020
 % Chapter 2 Excerise 2
-% Generation of data from exponential distribution using the inverse from uniform.
+% Generation of data from exponential distribution using the inverse from 
+% uniform.
 
+close all;
 clc;
 clear;
 
 n = 1000;
 lamda = 1;
-nbins = 20;
 
-x = rand(n,1); % uniformly distributed random variables
+x = rand(n,1);      % uniformly distributed random variables
 y = -(1/lamda)*log(1-x); 
 
 % Histogram of y and an exponential distribution fit
-histfit(y,nbins);
+nbins = 100;
+histfit(y,nbins,'exponential');
