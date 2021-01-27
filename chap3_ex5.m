4deee9ee6489ffee1bc260bb14c681ccbad9cd52
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-27 17:44:26 +0200

    Update chap3_ex5.m

--- chap3_ex5.m
+++ chap3_ex5.m
@@ -10,27 +10,30 @@ clear;
 
 eruption = importdata("eruption.dat");
 
+
 % compute 95% CI of standard deviation and mean of eruption
+[~,~,varCI,~] = vartest(eruption,1);
+stdCI = sqrt(varCI);
+fprintf('Confidence interval of standart deviation of waiting time in 1989 is [%0.3f %0.3f]\n',stdCI(1,1),stdCI(2,1));
+fprintf('Confidence interval of standart deviation of duration in 1989 is [%0.3f %0.3f]\n',stdCI(1,2),stdCI(2,2));
+fprintf('Confidence interval of standart deviation of waiting time in 2006 is [%0.3f %0.3f]\n',stdCI(1,3),stdCI(2,3));
+
+[~,~,meanCI,~] = ttest(eruption,1);
+fprintf('\nConfidence interval of mean of waiting time in 1989 is [%0.3f %0.3f]\n',meanCI(1,1),meanCI(2,1));
+fprintf('Confidence interval of mean of duration in 1989 is [%0.3f %0.3f]\n',meanCI(1,2),meanCI(2,2));
+fprintf('Confidence interval of mean of waiting time in 2006 is [%0.3f %0.3f]\n\n',meanCI(1,3),meanCI(2,3));
+
 % testing for goodness of fit
-varCI = zeros(3,2);
-meanCI = zeros(3,2);
 h = zeros(3,1);
 p = zeros(3,1);
 for i = 1:3
-    data = eruption(:,i);
-    [~,~,varCI(i,:),~] = vartest(data,var(data));
-    [~,~,meanCI(i,:),~] = ttest(data,mean(data));
-    [h(i),p(i)] = chi2gof(data);
+    [h(i),p(i)] = chi2gof(eruption(:,i));
     if h(i) == 1
         fprintf('The hypothesis that normal distribution fits is rejected\n')
     else
         fprintf('The hypothesis that normal distribution fits is accepted\n')
     end
 end
-stdCI = sqrt(varCI);
-fprintf('\nConfidence interval of standart deviation of waiting time in 1989 is [%0.3f %0.3f]\n',stdCI(1,1),stdCI(1,2));
-fprintf('Confidence interval of standart deviation of duration in 1989 is [%0.3f %0.3f]\n',stdCI(2,1),stdCI(2,2));
-fprintf('Confidence interval of standart deviation of waiting time in 2006 is [%0.3f %0.3f]\n',stdCI(3,1),stdCI(3,2));
 
 % hypothesis test that std = 10 for waiting time and std = 1 for duration
 [h1,~,~,~] = vartest(eruption(:,1),100);
@@ -47,10 +50,6 @@ else
 end
 
 % hypothesis test that mean = 75 for waiting time and mean = 2.5 for duration
-fprintf('\nConfidence interval of mean of waiting time in 1989 is [%0.3f %0.3f]\n',meanCI(1,1),meanCI(1,2));
-fprintf('Confidence interval of mean of duration in 1989 is [%0.3f %0.3f]\n',meanCI(2,1),meanCI(2,2));
-fprintf('Confidence interval of mean of waiting time in 2006 is [%0.3f %0.3f]\n',meanCI(3,1),meanCI(3,2));
-
 [h4,~,~,~] = ttest(eruption(:,1),75);
 if h4 == 1
     fprintf('\nThe hypothesis that mean = 75 for waiting time is rejected\n')
