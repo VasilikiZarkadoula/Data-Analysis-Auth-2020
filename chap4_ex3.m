4970f1a1d74f64a740f762f1705476bf5f03f7a9
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-27 18:53:17 +0200

    Update chap4_ex3.m

--- chap4_ex3.m
+++ chap4_ex3.m
@@ -1,56 +1,49 @@
 % Vasia Zarkadoula
 % Data Analysis 2020
 % Chapter 4 Excerise 3
-% Uncertainty in power dissipated in a circuit of alternating current
+% Uncertainty in power dissipated from an alternating current circuit
 
 clc;
 clear;
 
 % check if power has the expected accuracy
 % V,I,f are not correlated
-
 meanV = 77.78;
 stdV = 0.71;
 meanI = 1.21;
 stdI = 0.071;
 meanf = 0.283;
 stdf = 0.017;
+% law of propagation of errors
+sigmaP = sqrt((meanI*cos(meanf))^2*stdV^2 + (meanV*cos(meanf))^2*stdI^2 + (meanV*meanI*(-sin(meanf)))^2*stdf^2);
 
 M = 1000;
 
-V = zeros(1,M);
-I = zeros(1,M);
-f = zeros(1,M);
-P = zeros(1,M);
-for i = 1:M
-    V(i) = normrnd(meanV,stdV);
-    I(i) = normrnd(meanI,stdI);
-    f(i) = normrnd(meanf,stdf);
-    P(i) = V(i)*I(i)*cos(f(i));
-end
+V = normrnd(meanV,stdV,M,1);
+I = normrnd(meanI,stdI,M,1);
+f = normrnd(meanf,stdf,M,1);
+P = V.*I.*cos(f);
 
-meanP= mean(P);
+meanP = mean(P);
 stdP = std(P);
-sigmaP = sqrt((meanI*cos(meanf))^2*stdV^2 + (meanV*cos(meanf))^2*stdI^2 + (meanV*meanI*(-sin(meanf)))^2*stdf^2);
-fprintf('Expected STD for P = %2.3f   Observed STD for P = %2.3f \n',sigmaP,stdP);
+fprintf('Expected STD for P = %2.3f\nObserved STD for P = %2.3f \n',sigmaP,stdP);
 
 %--------------------------------------------------------------------------
 
 % check if power has the expected accuracy
 % V,f are correlated
-
 pVf = 0.5;
 sigmaPnew  = sqrt(sigmaP + 2*meanI*cos(meanf)*meanV*meanI*(-sin(meanf))*stdV*stdf*pVf);
 
 meanAll = [meanI meanV meanf]';
-covvf = pVf*stdV*stdf;
-sigmaAll = [stdI^2 0 0; 0 stdV^2 covvf; 0 covvf stdf^2];
+covVF = pVf*stdV*stdf;
+sigmaAll = [stdI^2 0 0; 0 stdV^2 covVF; 0 covVF stdf^2];
 x = mvnrnd(meanAll,sigmaAll,M); 
+
 i = x(:,1);
 v = x(:,2);
 f = x(:,3);
+
 p = v.*i.*cos(f);
-stdp = std(p);
-fprintf('Expected SD for P = %2.3f   Observed SD for P = %2.3f \n',sigmaPnew,stdp);
-
-
+pStd = std(p);
+fprintf('\nExpected SD for P = %2.3f\nObserved SD for P = %2.3f \n',sigmaPnew,pStd);
