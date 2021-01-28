60e48ec9ee55ce2c8eefc856fb0deba304796152
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-28 16:12:52 +0200

    Update chap5_ex5.m

--- chap5_ex5.m
+++ chap5_ex5.m
@@ -8,40 +8,51 @@ clear;
 
 % import data
 lightair = importdata('lightair.dat');
-x = lightair(:,1);
-y = lightair(:,2);
+n = length(lightair);
 
 % 1. new bootstrap samples
-bootSamples = bootstrp(100,@mean,lightair);
+bootSamples = lightair(unidrnd(n,n,1), :);
 
 % 2. bo,b1 estimates
-bootX = bootSamples(:,1);
-bootY = bootSamples(:,2);
-linearModel = fitlm(bootX,bootY); 
+linearModel = fitlm(bootSamples(:,1),bootSamples(:,2)); 
 b = linearModel.Coefficients.Estimate;
 b0 = b(1);
 b1 = b(2);
 
 % 3. repeat 1 and 2 1000 times
-M = 100;
-n = length(x);
+M = 1000;
 alpha = 0.05;
-b0Boot = zeros(M,1);
-b1Boot = zeros(M,1);
+
+b0Bootstrap = zeros(M,1);
+b1Bootstrap = zeros(M,1);
 for i = 1:M
-    bootSamples = bootstrp(100,@mean,lightair);
+    bootSamples = lightair(unidrnd(n,n,1), :);
     linearModel = fitlm(bootSamples(:,1),bootSamples(:,2)); 
     b = linearModel.Coefficients.Estimate;
-    b0Boot(i) = b(1);
-    b1Boot(i) = b(2);
+    b0Bootstrap(i) = b(1);
+    b1Bootstrap(i) = b(2);
 end
 
-% 4. confidence intervals for b0,b1
-b0Boot = sort(b0Boot);
-b1Boot = sort(b1Boot);
+% 4. non- parametric confidence intervals for b0,b1
+b0Bootstrap = sort(b0Bootstrap);
+b1Bootstrap = sort(b1Bootstrap);
 lowLim = round(M*alpha/2);
 uppLim = round(M*(1-alpha/2));
-b0bootCI = [b0Boot(lowLim) b0Boot(uppLim)];
-b1bootCI = [b1Boot(lowLim) b1Boot(uppLim)];
 
+b0bootCI = [b0Bootstrap(lowLim) b0Bootstrap(uppLim)];
+b1bootCI = [b1Bootstrap(lowLim) b1Bootstrap(uppLim)];
+fprintf('Non-Parametric CI of bo = [%2.3f %2.3f] \n',b0bootCI(1,:));
+fprintf('Non-Parametric CI of b1 = [%2.3f %2.3f] \n',b1bootCI(1,:));
 
+% percentiles = [5/2 100-5/2];
+% b0CI = prctile(b0Bootstrap,percentiles);
+% b1CI = prctile(b1Bootstrap,percentiles);
+
+% parametric confidence intervals for b0,b1
+% Parametrical
+X = lightair(:,1);
+Y = lightair(:,2);
+regressionModel = fitlm(X,Y);
+ci = coefCI(regressionModel);
+fprintf('\nParametric CI of bo = [%2.3f %2.3f] \n',ci(1,:));
+fprintf('Parametric CI of b1 = [%2.3f %2.3f] \n',ci(2,:));
