bc6f71b05c16a34ff591de3b95187c6e19128e1c
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-28 19:24:47 +0200

    Update chap6_ex3.m

--- chap6_ex3.m
+++ chap6_ex3.m
@@ -7,38 +7,41 @@ clc;
 clear;
 
 data = importdata('physical.txt');
+data = data(:,2:end);
 
 % a) estimate new dimension
 
 dataCentered = data - mean(data);
-[eigenvectors,score,eigenvalues] = pca(dataCentered);
+[~,scores,eigenvalues] = pca(dataCentered);
+
 m = mean(eigenvalues);
-threshold = 0.7*m;
+threshold = 0.5*m;
+dim = find(eigenvalues>threshold);
+newDimension = max(dim);
+
 % scree plot
+figure;
 plot(1:length(eigenvalues),eigenvalues,'ko-');
 hold on 
 plot( xlim, [threshold threshold] )
 title('Scree Plot')
 xlabel('Index')
 ylabel('eigenvalue')
+hold off
 
-dim = find(eigenvalues>threshold);
-newDimension = max(dim);
 
 %--------------------------------------------------------------------------
 
 % b) plot 2d and 3d PCA components
  
-score2d = dataCentered*eigenvectors(:,1:2);
-figure(1)
-scatter(score2d(:,1),score2d(:,2))
+figure;
+scatter(scores(:,1),scores(:,2))
 xlabel('PC1')
 ylabel('PC2')
 title('2d Principal component scores')
 
-score3d = dataCentered*eigenvectors(:,1:3);
-figure(2)
-scatter3(score3d(:,1),score3d(:,2),score3d(:,3))
+figure;
+scatter3(scores(:,1),scores(:,2),scores(:,3))
 xlabel('PC1')
 ylabel('PC2')
 zlabel('PC3')
