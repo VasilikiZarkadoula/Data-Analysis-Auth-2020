d9f53ffe7aa4c0b7cab18f20504b616ab2e53166
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-28 17:34:40 +0200

    Update chap6_ex1.m

--- chap6_ex1.m
+++ chap6_ex1.m
@@ -12,18 +12,21 @@ w = [0.2 0.8; 0.4 0.5; 0.7 0.3];
 n = 1000;
 
 x2d = mvnrnd(mu,sigma,n);
+x2dCentered = x2d - mean(x2d);
+
 x3d = x2d*w';
-xCentered = x3d - mean(x3d);
+x3dCentered = x3d - mean(x3d);
 
 % a) eigenvalues, eigenvectors and pca scores
 
-covX = cov(xCentered);
+covX = cov(x3dCentered);
 [eigenvectors,eigenvalues] = eig(covX);
 eigenvalues = diag(eigenvalues);
 [eigenvalues,ind] = sort(eigenvalues,'descend');
 eigenvectors = eigenvectors(:, ind);
 
-score3d = xCentered*eigenvectors;
+score3d = x3dCentered*eigenvectors;
+[~,scores,~] = pca(x3dCentered);
 
 figure(1)
 scatter3(score3d(:,1),score3d(:,2),score3d(:,3))
@@ -47,16 +50,14 @@ ylabel('eigenvalue')
 % c) Initial data in 2d plot and 2d PCA components
  
 figure(3)
-plot(x3d(:,1),x3d(:,2),'.')
+scatter(x2dCentered(:,1),x2dCentered(:,2))
 xlabel('x1')
 ylabel('x2')
-title('2D')
+title('2D Scatteplot')
 
 figure(4)
-score2d = xCentered*eigenvectors(:,1:2);
+score2d = x3dCentered*eigenvectors(:,1:2);
 scatter(score2d(:,1),score2d(:,2))
 xlabel('PC1')
 ylabel('PC2')
 title('Principal component scores')
-
-
